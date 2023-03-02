d01a47883c644e1a111e660b00c29a0482dade8b
Kyunghwan Kim <kkh147.17.3@gmail.com>
2023-03-02 11:03:12 +0900

    feat: add custom query creation method

--- src/main/java/com/podolist/podolist/repository/JPATaskRepository.java
+++ src/main/java/com/podolist/podolist/repository/JPATaskRepository.java
@@ -1,9 +1,32 @@
 package com.podolist.podolist.repository;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.podolist.podolist.Entity.Task;
 
-public interface JPATaskRepository extends JpaRepository<Task, Integer> {
+public interface JPATaskRepository extends JpaRepository<Task, Long> {
     
+    List<Task> findByStartedAtBetweenAndDeletedIs(LocalDateTime d1, LocalDateTime d2, boolean deleted);
+
+    default List<Task> getAllTasksAtDate(LocalDateTime date) {
+        var startedDate = date.with(LocalTime.of(0, 0, 0, 0));
+        var endDate = date.with(LocalTime.of(23, 59, 59, 999));
+        // Calendar startDateCalendar=Calendar.getInstance();
+        // Calendar endDateCalendar=Calendar.getInstance();
+        
+        // startDateCalendar.setTime(date);
+        // endDateCalendar.setTime(date);
+        // startDateCalendar.set(Calendar.HOUR_OF_DAY, 0);
+        // endDateCalendar.set(Calendar.HOUR_OF_DAY, 0);
+        // endDateCalendar.add(Calendar.DATE, 1);
+        // Date startDate=startDateCalendar.getTime();
+        // Date endDate=endDateCalendar.getTime();
+        return findByStartedAtBetweenAndDeletedIs(startedDate, endDate,false);
+    }
 }
